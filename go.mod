617f25c8498223c22e7a032333c383a583f349ac
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2022-11-23 19:39:42 +0900

    New:外部パッケージを使用して秘密分散法を実装
    秘密分散法がおおむね動作していることを確認した

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/GR-PasswordManager/GR-PasswordManager
 go 1.19
 
 require (
+	github.com/codahale/sss v0.0.0-20160501174526-0cb9f6d3f7f1
 	github.com/joho/godotenv v1.4.0
 	go.bug.st/serial v1.4.0
 )
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/codahale/sss"
+
+	"github.com/GR-PasswordManager/GR-PasswordManager/cmd/gr"
+)
+
+func main(){
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	env := os.Getenv("ENV")
+	fmt.Println("MODE:" + env)
+
+
+	switch env {
+		case "terminal":
+			gr.Terminal()
+		case "dev":
+			// 開発用
+			n := 5
+			k := 3
+			secret := "secret"
+			shares, err := sss.Split(byte(n), byte(k), []byte(secret))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(shares)
+
+			recov := sss.Combine(shares)
+			fmt.Println(recov)
+			fmt.Println(string(recov))
+
+		default:
+			log.Fatal("Error: No mode selected")
+	}
+}
