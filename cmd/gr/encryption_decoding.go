7dfa3c71623256c9f5091707d8adb8fc6d533e64
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2022-12-10 19:39:12 +0900

    Update:ハッシュを使用する形に変更
    各種シェア等が何番のものか管理できるようにハッシュに変更

--- cmd/gr/encryption_decoding.go
+++ cmd/gr/encryption_decoding.go
@@ -7,7 +7,7 @@ import (
 	ecies "github.com/ecies/go/v2"
 )
 
-func Encrypt(n int, k int, secret string, publickeys []*ecies.PublicKey) ([]*ecies.PrivateKey, [][]byte) {
+func Encrypt(n int, k int, secret string, publickeys map[byte]*ecies.PublicKey) (map[byte]*ecies.PrivateKey, map[byte][]byte) {
 	// 分散シェアの生成
 	shares, err := sss.Split(byte(n), byte(k), []byte(secret))
 	if err != nil {
@@ -16,28 +16,28 @@ func Encrypt(n int, k int, secret string, publickeys []*ecies.PublicKey) ([]*eci
 	log.Println("shares: ", shares)
 
 	// 分散シェアの暗号化
-	var encrypted_share [][]byte = nil
-	keys := make([]*ecies.PrivateKey, n)
+	encrypted_share := map[byte][]byte{}
+	keys := map[byte]*ecies.PrivateKey{}
 
 	if publickeys == nil {
 		log.Println("publickeys is nil")
-		publickeys = make([]*ecies.PublicKey, n)
-		for i := 0; i < n; i++ {
-			keys[i], err = ecies.GenerateKey()
+		publickeys = map[byte]*ecies.PublicKey{}
+		for i := 1; i <= n; i++ {
+			keys[byte(i)], err = ecies.GenerateKey()
 			if err != nil {
 				panic(err)
 			}
 			log.Println("key[", i, "] pair has been generated")
-			publickeys[i] = keys[i].PublicKey
+			publickeys[byte(i)] = keys[byte(i)].PublicKey
 		}
 	}
 
-	for i := 0; i < n; i++ {
-		ciphertext, err := ecies.Encrypt(publickeys[i], shares[byte(i+1)])
+	for i := 1; i <= n; i++ {
+		ciphertext, err := ecies.Encrypt(publickeys[byte(i)], shares[byte(i)])
 		if err != nil {
 			panic(err)
 		}
-		encrypted_share = append(encrypted_share, ciphertext)
+		encrypted_share[byte(i)] = ciphertext
 	}
 
 	return keys, encrypted_share
