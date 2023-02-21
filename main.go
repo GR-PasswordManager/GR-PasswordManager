c1f3d39baaa0589da7b8da12cde94ce4da6fd9f0
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2023-02-21 21:09:10 +0900

    New:発表用の状態に変更
    発表用にフォルダへのファイル書き出しを行い視覚的にわかりやすいように変更

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"crypto/aes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/codahale/sss"
 	ecies "github.com/ecies/go/v2"
@@ -98,6 +102,142 @@ func main(){
 				plain_shares[byte(i)], err = ecies.Decrypt(keys[byte(i)], shares[byte(i)])
 				if err != nil {
 					panic(err)
+				}
+				log.Println("ciphertext decrypted: ", plain_shares[byte(i)])
+			}
+
+			decrypt_text := gr.Decrypt(plain_shares, block)
+			log.Println(string(decrypt_text))
+
+		case "dev-presentation":
+			// プレゼンテーション用
+			// ターミナルからの入力を受け取り、AESによる暗号化し分散、各デバイスに転送可能な状態にする。
+			// ディレクトリを作成し、デバイスに転送する予定のファイルを作成する。
+			// 一時停止
+			// また、複合・結合を行い、ターミナルに出力する。
+
+			// 分散シェアの生成
+			k := 0;
+			n := 0;
+
+			fmt.Print("Input k:")
+			fmt.Scan(&k)
+			fmt.Print("Input n:")
+			fmt.Scan(&n)
+
+			if k > n {
+				log.Fatal("Error: k > n")
+			}else if k < 1 {
+				log.Fatal("Error: k < 1")
+			}else if n < 1 {
+				log.Fatal("Error: n < 1")
+			}
+
+			secret := ""
+
+			fmt.Print("Input secret:")
+			fmt.Scan(&secret)
+
+			keys := map[byte]*ecies.PrivateKey{}
+			publickeys := map[byte]*ecies.PublicKey{}
+			for i := 1; i <= n; i++ {
+				keys[byte(i)], err = ecies.GenerateKey()
+				if err != nil {
+					panic(err)
+				}
+				publickeys[byte(i)] = keys[byte(i)].PublicKey
+			}
+
+			block, err := aes.NewCipher([]byte("12345678901234561234567890123456"))
+			if err != nil {
+				panic(err)
+			}
+
+			shares := gr.Encrypt(k, n, []byte(secret), block, publickeys)
+
+			log.Println(shares)
+
+			// ディレクトリ作成
+			dir := "presentation_" + time.Now().Format("2006-1-2_15-04")
+			err = os.Mkdir(dir, 0777)
+
+			// 未接続のドングル指定
+			dongle := map[byte]string{}
+			fmt.Println("未接続のドングルを指定してください。")
+			for i := 1; i <= n; i++ {
+				fmt.Println("ドングル" + strconv.Itoa(i) + "は接続していますか？(y/n)")
+				var yn string
+				fmt.Scan(&yn)
+				if yn == "y" || yn == "n" {
+					dongle[byte(i)] = string(yn)
+				} else {
+					log.Fatal("Error: y or n")
+				}
+			}
+
+			// ディレクトリ作成
+			for i := 1; i <= n; i++ {
+				if string(dongle[byte(i)]) == "y" {
+					err = os.Mkdir(dir + "/" + strconv.Itoa(i), 0777)
+				}
+			}
+
+			// ファイル作成
+			for i := 1; i <= len(shares); i++ {
+				if f, err := os.Stat(dir + "/" + strconv.Itoa(i)); os.IsNotExist(err) || !f.IsDir() {
+					fmt.Println("ドングル" + strconv.Itoa(i) + "は接続していません。")
+					for j := 1; j <= n; j++ {
+						if string(dongle[byte(j)]) == "y" {
+							file, err := os.Create(dir + "/" + strconv.Itoa(j) + "/share_" + strconv.Itoa(i) + ".share")
+							if err != nil {
+								panic(err)
+							}
+							defer file.Close()
+
+							file.Write(shares[byte(i)])
+						}
+					}
+				} else {
+					file, err := os.Create(dir + "/" + strconv.Itoa(i) + "/share_" + strconv.Itoa(i) + ".share")
+					if err != nil {
+						panic(err)
+					}
+					defer file.Close()
+
+					file.Write(shares[byte(i)])
+				}
+			}
+
+			// 一時停止
+			fmt.Println("Press Enter to continue...")
+			bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+			shares = map[byte][]byte{}
+
+			// ファイル読み込み
+			for i := 1; i <= n; i++ {
+				file, err := os.Open(dir + "/" + strconv.Itoa(i) + "/share_" + strconv.Itoa(i) + ".share")
+				if err != nil {
+					log.Println("no such file or directory :" + dir + "/" + strconv.Itoa(i) + "/share_" + strconv.Itoa(i) + ".share")
+					continue
+				}
+				defer file.Close()
+
+				shares[byte(i)], err = ioutil.ReadAll(file)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			log.Println(shares)
+
+			// 分散シェアの復号化
+			plain_shares := map[byte][]byte{}
+			for i := 1; i <= n; i++ {
+				plain_shares[byte(i)], err = ecies.Decrypt(keys[byte(i)], shares[byte(i)])
+				if err != nil {
+					log.Println("failed to decrypt ciphertext: ", err, "share_" + "/" + strconv.Itoa(i) + "/share_" + strconv.Itoa(i) + ".share")
+					delete(plain_shares, byte(i))
 				}
 				log.Println("ciphertext decrypted: ", plain_shares[byte(i)])
 			}
